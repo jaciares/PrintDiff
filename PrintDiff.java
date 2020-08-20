35cac75e94517d516298fddb7f22afb0639565b2
jaciares <jwaciares@gmail.com>
2020-08-19 22:37:34 -0400

    hands on lab exercise

---
+++ PrintDiff.java
@@ -0,0 +1,18 @@
+/**
+ * Print Experiment.
+ * John Aciares
+ * CMIS 141
+ * 08/19/2020
+ */
+public class PrintDiff {
+    public static void main(String[] args)
+    {
+        System.out.println("a       a");
+        System.out.println("bb       bb");
+        System.out.println("ccc       ccc");
+        System.out.println("dddd       dddd");
+        System.out.println("ccc       ccc");
+        System.out.println("bb      bb");
+        System.out.println("a      a");
+    }
+}
